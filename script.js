59a054c4cbab3447491f1125ea65dffad621196a
basilbenny1002 <basilbenny1002@gmail.com>
2026-01-10 00:50:59 +0530

    Add seconds to the timer when the user has spend mroe than an hr with a task

--- script.js
+++ script.js
@@ -593,7 +593,7 @@ function formatTime(seconds) {
     const h = Math.floor(seconds / 3600);
     const m = Math.floor((seconds % 3600) / 60);
     const s = seconds % 60;
-    if (h > 0) return `${h}h ${m}m`;
+    if (h > 0) return `${h}h ${m}m ${s}s`;
     return `${m}m ${s}s`;
 }
 
